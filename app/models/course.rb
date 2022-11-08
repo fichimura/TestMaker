8567770d28355a5ce4ab616ece11f68990b0ba9c
fichimura <fabichimura@gmail.com>
2022-11-08 01:30:25 -0300

    user courses relation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,11 @@
 class Course < ApplicationRecord
     validates :titulo,  presence: true
     validates :descricao, presence: true, length: { :minimum => 5 }
+    
+    belongs_to :user
     def to_s
         titulo
     end
-    has_rich_text :descricao
+   
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+#User.create!(email: 'admin@example.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
+
 30.times do
     Course.create!([{
       titulo: Faker::Educator.course_name,
-      descricao: Faker::TvShows::GameOfThrones.quote
+      descricao: Faker::TvShows::GameOfThrones.quote,
+      user_id: User.first.id
     }])
   end
